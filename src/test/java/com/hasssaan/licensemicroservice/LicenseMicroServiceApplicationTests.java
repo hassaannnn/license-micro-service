4cc6cd91f4c1769b2ed8802019cd6372f860b4fd
has <hassaan3@ualberta.ca>
2022-11-15 13:43:04 -0700

    iniital commit of license

---
+++ src/test/java/com/hasssaan/licensemicroservice/LicenseMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasssaan.licensemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
